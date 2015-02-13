ed45e45cd4ee03e2e76aa099dffcedd003854100
shinsuke.a.yamada <shinsuke.a.yamada@mail.rakuten.com>
2015-02-13 13:35:46 +0900

    update _create & _init prosess of View, fix bug of _delegateEvent of View.

--- src/view.js
+++ src/view.js
@@ -16,11 +16,14 @@ window.$MR.View = (function($, $MR) {
         _create: function(options) {
             this.el = options.el;
             this.$el = $(this.el);
+            this.model = options.model;
             this.settings = $.extend({}, this.defaults, options, this.$el.data('options'));
             this._init();
         },
 
-        _init: function() {},
+        _init: function() {
+            this._delegate();
+        },
 
         _mkCache: function() {
             this.settings.cache = {
@@ -60,7 +63,7 @@ window.$MR.View = (function($, $MR) {
                 var args = eventName.split(' '),
                     argsLen = args.length,
                     _handler = self._bind(handler),
-                    isHasChildElement = tagElemsRegexp.test(args[argsLen-1].toLowerCase());
+                    isHasChildElement = (argsLen > 1) && tagElemsRegexp.test(args[argsLen-1].toLowerCase());
 
                 if (isHasChildElement) {
                     args = [args.slice(0, argsLen-1).join(' '), args[argsLen-1]];
